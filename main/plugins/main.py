6fa4aa9d8a5fd32789b44c9362656bf8532bb3f6
milad0919a <96024781+milad0919a@users.noreply.github.com>
2022-10-06 03:03:02 +0330

    Initialize main

---
+++ main/plugins/main.py
@@ -0,0 +1,191 @@
+#Tg:ChauhanMahesh/Dronebots
+#Github.com/vasusen-code
+
+import os
+from .. import Drone 
+from telethon import events, Button
+from main.plugins.rename import media_rename
+from main.plugins.compressor import compress
+from main.plugins.trimmer import trim
+from main.plugins.convertor import mp3, flac, wav, mp4, mkv, webm, file, video
+
+@Drone.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
+async def compin(event):
+    if event.is_private:
+        media = event.media
+        if media:
+            video = event.file.mime_type
+            if 'video' in video:
+                await event.reply("📽",
+                            buttons=[
+                                [Button.inline("COMPRESS", data="compress"),
+                                 Button.inline("CONVERT", data="convert")],
+                                [Button.inline("RENAME", data="rename"),
+                                 Button.inline("TRIM", data="trim")]
+                            ])
+            elif 'png' in video:
+                return
+            elif 'jpeg' in video:
+                return
+            elif 'jpg' in video:
+                return    
+            else:
+                await event.reply('📦',
+                            buttons=[  
+                                [Button.inline("RENAME", data="rename")]])
+
+                                             
+@Drone.on(events.callbackquery.CallbackQuery(data="convert"))
+async def convert(event):
+    await event.edit("🔃**CONVERT:**",
+                    buttons=[
+                        [Button.inline("MP3", data="mp3"),
+                         Button.inline("FLAC", data="flac"),
+                         Button.inline("WAV", data="wav")],
+                        [Button.inline("MP4", data="mp4"),
+                         Button.inline("WEBM", data="webm"),
+                         Button.inline("MKV", data="mkv")],
+                        [Button.inline("FILE", data="file"),
+                         Button.inline("VIDEO", data="video")],
+                        [Button.inline("BACK", data="back")]])
+                        
+@Drone.on(events.callbackquery.CallbackQuery(data="back"))
+async def back(event):
+    await event.edit("📽",
+                    buttons=[
+                        [Button.inline("COMPRESS", data="compress"),
+                         Button.inline("CONVERT", data="convert")],
+                        [Button.inline("RENAME", data="rename"),
+                         Button.inline("TRIM", data="trim")]])
+                            
+#-----------------------------------------------------------------------------------------
+
+@Drone.on(events.callbackquery.CallbackQuery(data="mp3"))
+async def vtmp3(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    if not os.path.isdir("audioconvert"):
+        await event.delete()
+        os.mkdir("audioconvert")
+        await mp3(event, msg)
+        os.rmdir("audioconvert")
+    else:
+        await event.edit("Another process in progress!")
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="flac"))
+async def vtflac(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message()  
+    if not os.path.isdir("audioconvert"):
+        await event.delete()
+        os.mkdir("audioconvert")
+        await flac(event, msg)
+        os.rmdir("audioconvert")
+    else:
+        await event.edit("Another process in progress!")
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="wav"))
+async def vtwav(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    if not os.path.isdir("audioconvert"):
+        await event.delete()
+        os.mkdir("audioconvert")
+        await wav(event, msg)
+        os.rmdir("audioconvert")
+    else:
+        await event.edit("Another process in progress!")
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="mp4"))
+async def vtmp4(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    await mp4(event, msg)
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="mkv"))
+async def vtmkv(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    await mkv(event, msg)  
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="webm"))
+async def vtwebm(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    await webm(event, msg)  
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="file"))
+async def vtfile(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    await file(event, msg)    
+
+@Drone.on(events.callbackquery.CallbackQuery(data="video"))
+async def ftvideo(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    await video(event, msg)
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="rename"))
+async def rename(event):                            
+    button = await event.get_message()
+    msg = await button.get_reply_message()  
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        cm = await conv.send_message("Send me a new name for the file as a `reply` to this message.\n\n**NOTE:** `.ext` is not required.")                              
+        try:
+            m = await conv.get_reply()
+            new_name = m.text
+            await cm.delete()                    
+            if not m:                
+                return await cm.edit("No response found.")
+        except Exception as e: 
+            print(e)
+            return await cm.edit("An error occured while waiting for the response.")
+    await media_rename(event, msg, new_name)                     
+                   
+@Drone.on(events.callbackquery.CallbackQuery(data="compress"))
+async def compresss(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message()  
+    if not os.path.isdir("compressmedia"):
+        await event.delete()
+        os.mkdir("compressmedia")
+        await compress(event, msg)
+        os.rmdir("compressmedia")
+    else:
+        await event.edit("Another process in progress!")
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="trim"))
+async def vtrim(event):                            
+    button = await event.get_message()
+    msg = await button.get_reply_message()  
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        try:
+            xx = await conv.send_message("send me the start time of the video you want to trim from as a reply to this. \n\nIn format hh:mm:ss , for eg: `01:20:69` ")
+            x = await conv.get_reply()
+            st = x.text
+            await xx.delete()                    
+            if not st:               
+                return await xx.edit("No response found.")
+        except Exception as e: 
+            print(e)
+            return await xx.edit("An error occured while waiting for the response.")
+        try:
+            xy = await conv.send_message("send me the end time of the video you want to trim till as a reply to this.  \n\nIn format hh:mm:ss , for eg: `01:20:69` ")  
+            y = await conv.get_reply()
+            et = y.text
+            await xy.delete()                    
+            if not et:                
+                return await xy.edit("No response found.")
+        except Exception as e: 
+            print(e)
+            return await xy.edit("An error occured while waiting for the response.")
+        await trim(event, msg, st, et)
+            
---
+++ main/plugins/start.py
@@ -0,0 +1,111 @@
+#tg:ChauhanMahesh/DroneBots
+#github.com/vasusen-code
+
+import os
+from .. import Drone
+from telethon import events, Button
+from LOCAL.localisation import START_TEXT as st
+from LOCAL.localisation import JPG0 as file
+from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
+from ethon.teleutils import mention
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', 
+                      buttons=[
+                              [Button.inline("Menu.", data="menu")]
+                              ])
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="menu"))
+async def menu(event):
+    await event.client.send_file(event.chat_id, caption="**📑MENU.**", file=file,
+                    buttons=[[
+                         Button.inline("info.", data="info"),
+                         Button.inline("SOURCE-CODE", data="source")],
+                         [
+                         Button.inline("NOTICE.", data="notice"),
+                         Button.inline("Help/SETTINGS.", data="help")],
+                         [
+                         Button.url("DEVELOPER", url=f"{DEV}")]])
+    await event.delete()
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="menu2"))
+async def menu2(event):
+    await event.edit("**📑MENU.**",
+                    buttons=[[
+                         Button.inline("info.", data="info"),
+                         Button.inline("SOURCE-CODE", data="source")],
+                         [
+                         Button.inline("NOTICE.", data="notice"),
+                         Button.inline("Help/SETTINGS.", data="help")],
+                         [
+                         Button.url("DEVELOPER", url=f"{DEV}")]])
+       
+@Drone.on(events.callbackquery.CallbackQuery(data="info"))
+async def info(event):
+    await event.edit(f'**ℹ️NFO:**\n\n{info_text}',
+                    buttons=[[
+                         Button.inline("Menu.", data="menu2")]])
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="notice"))
+async def notice(event):
+    await event.answer(f'{spam_notice}', alert=True)
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="source"))
+async def source(event):
+    await event.edit(source_text,
+                    buttons=[[
+                         Button.url("Main.", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
+                         Button.url("PUBLIC", url="https://github.com/vasusen-code/videoconvertor/tree/public")]])
+                         
+                    
+@Drone.on(events.callbackquery.CallbackQuery(data="help"))
+async def help(event):
+    await event.edit('**👥HELP & SETTINGS.**',
+                    buttons=[[
+                         Button.inline("set THUMBNAIL.", data="sett"),
+                         Button.inline("rem THUMBNAIL.", data='remt')],
+                         [
+                         Button.inline("PLUGUNS..", data="plugins"),
+                         Button.url("SUPPORT.", url=f"{SUPPORT_LINK}")],
+                         [
+                         Button.inline("Menu.", data="menu2")]])
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="plugins"))
+async def plugins(event):
+    await event.edit(f'{help_text}',
+                    buttons=[[
+                         Button.inline("Menu.", data="menu2")]])
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="sett"))
+async def sett(event):    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        if os.path.exists(f'{event.sender_id}.jpg'):
+            os.remove(f'{event.sender_id}.jpg')
+        os.rename(path, f'./{event.sender_id}.jpg')
+        await t.edit("Temporary thumbnail saved!")
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="remt"))
+async def remt(event):  
+    await event.edit('Trying.')
+    try:
+        os.remove(f'{event.sender_id}.jpg')
+        await event.edit('Removed!')
+    except Exception:
+        await event.edit("No thumbnail saved.")
+    
